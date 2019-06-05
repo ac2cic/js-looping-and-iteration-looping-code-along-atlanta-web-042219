0029d142f583c7841e66d8327cd5ee8220b6118d
github username <github email address>
2019-06-05 14:09:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+document.addEventListener('DOMContentLoaded', () => {
+  writeCards(['andrew', 'jae'], ', fancy')
+})
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   writeCards(['andrew', 'jae'], ', fancy')
+// })
+
+function writeCards(people, gift) {
+  let cards = []
+  for (let i = 0; i < people.length; i++) {
+    cards.push(`Thank you, ${people[i]}, for the wonderful ${gift} gift!`)
+  }
+  console.log(cards)
+  return cards
+}
+
+function countdown(number) {
+  for (let i = number; i >= 0; i-- ) {
+  console.log(i)
+  }
+}
